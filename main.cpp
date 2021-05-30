d6ca8b45f953a451ddfc7a5d8a337ac42c84ea85
Mark Kenneth Ferrer <81341766+Dev-Kenn@users.noreply.github.com>
2021-05-30 14:32:02 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int prelim = 85, midterm = 93, final = 87;
+	int average;
+	
+	average = (85 + 93 + 87) /3;
+	cout <<"Prelim = 85, Midterm = 93, Final = 87\n\n";
+	cout <<"The average is: " <<average;
+	
+	return 0;
+}
